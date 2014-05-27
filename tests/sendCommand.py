c53fdf9c9262112bf7fbd1831e76c6ea695d0cf5
Eddy Hintze <eddy.hintze@gmail.com>
2014-05-27 10:40:18 -0400

    Created tests for the new arduino functions

--- tests/sendCommand.py
+++ tests/sendCommand.py
@@ -4,25 +4,125 @@ import telnetlib
 
 
 
-IP = "192.168.1.161"
-PORT = 2000
+class neoLed:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
 
+    def cmd(self,cmdList):
+        self.cmd = ":"
+        for value in cmdList:
+            self.cmd += value + ":"
+        self.sendCmd()
 
-def sendCmd(cmd):
-	tn = telnetlib.Telnet(IP, PORT)
-	tn.read_until(b"*HELLO*", 2)
-	tn.write(cmd)
-	time.sleep(.5)
-	tn.close()
+    def sendCmd(self):
+        tn = telnetlib.Telnet(self.ip, self.port)
+        tn.read_until(b"*HELLO*", 2)
+        tn.write(self.cmd)
+        tn.close()
 
 
 
 
 if __name__ == "__main__":
-	cmd = ":"
-	for arg in sys.argv:
-		if arg != sys.argv[0]:
-			cmd += arg + ":"
-	sendCmd(cmd)
+    # solid = [
+    #             action,
+    #             cycle,
+    #             strip,
+    #             startled,
+    #             length,
+    #             delay,
+    #             red,
+    #             green,
+    #             blue,
+    #             dir,
+    #             ]
+    solid = [
+            "solid",
+            "0",
+            "0",
+            "1",
+            "20",
+            "60",
+            "0",
+            "127",
+            "127",
+            "0",
+            ]
+    # rCycle = [
+    #          action,
+    #          cycle,
+    #          ledstrip,
+    #          startled,
+    #          length,   #number of leds to use
+    #          delay,
+    #          Color1 r,
+    #          Color1 g,
+    #          Color1 b,
+    #          Color2 r,
+    #          Color2 g,
+    #          Color2 b,
+    #          dir,
+    #          ]
+    rCycle = [
+             "rCycle",
+             "1",
+             "0",
+             "1",
+             "30",
+             "100",
+
+             "255",
+             "0",
+             "0",
+
+             "0",
+             "255",
+             "0",
+
+             "0",
+             ]
+    # slide = [
+    #         action,
+    #         cycle,
+    #         ledstrip,
+    #         startLed,
+    #         length,   #number of leds to use
+    #         delay,
+    #         Color1 r, #slide color
+    #         Color1 g,
+    #         Color1 b,
+    #         Color2 r, #background color
+    #         Color2 g,
+    #         Color2 b,
+    #         slide length,
+    #         dir,
+    #         ]
+    slide = [
+            "slide",
+            "1",
+            "0",
+            "1",
+            "10",
+            "100",
+
+            "0",
+            "0",
+            "255",
+
+            "0",
+            "255",
+            "0",
+
+            "1",
+            "0",
+            ]
+    strip = neoLed("192.168.1.161", 2000)
+    #strip.cmd(solidList)
+    #time.sleep(3)
+    #strip.cmd(rainbowCycleList)
+    strip.cmd(rCycle)
+
+
 
 
